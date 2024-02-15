6018e378dbbc602bcb46fdca656e20f94823d188
NikhilCode12 <promaxchessace@gmail.com>
2024-02-15 21:44:18 +0530

    add: font-rubik for typography

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { Rubik, Outfit } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   const childrenHeight = `calc(100vh - 4rem)`;
   return (
     <html lang="en" className="h-[100vh]">
-      <body className={inter.className}>
+      <body className={rubik.className}>
         <div className="h-16">
           <Header />
         </div>
---
+++ app/utils/constants.js
@@ -0,0 +1,3 @@
+export default {
+  desc: "Drag and drop a file here or click to upload",
+};
