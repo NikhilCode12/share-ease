ce5382144686eb15fd84eb99788f8c73a6eb07d5
NikhilCode12 <promaxchessace@gmail.com>
2024-02-15 21:45:54 +0530

    fix: border-styling header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
   }, [activePage, newPath]);
 
   return (
-    <header className="bg-gray-800 border-b h-full">
+    <header className="bg-gray-800 border-b h-full border-gray-600">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 py-2">
         <div className="flex items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
